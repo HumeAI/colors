f700b00cae5626b863d16662647a94cab8e9144e
Zach Krall <zach@zachkrall.com>
2024-04-16 13:04:50 -0400

    add json version of color map (#3)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from "tsup";
+import fs from "fs";
+import path from "path";
+
+const rootDir = path.resolve(process.cwd());
 
 export default defineConfig([
   {
@@ -7,5 +11,10 @@ export default defineConfig([
     external: [],
     dts: true,
     sourcemap: true,
+    onSuccess: async () => {
+      const jsonFile = path.resolve(rootDir, "lib", "generated", "colors.json");
+      
+      fs.copyFileSync(jsonFile, path.resolve(rootDir, "dist", "colors.json"));
+    }
   },
 ]);
